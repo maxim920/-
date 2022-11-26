c7b002c368dd4c3e771de83339633d331dff2694
maxim920 <90177561+maxim920@users.noreply.github.com>
2022-11-26 23:54:22 +0200

    Create лб13_1.py

---
+++ лб13_1.py
@@ -0,0 +1,81 @@
+import math 
+
+ 
+
+import numpy as np 
+
+ 
+
+import matplotlib.pyplot as plt 
+
+ 
+
+def f(x,y): 
+
+ 
+
+    return x + math.sin(y/3) 
+
+ 
+
+x0 = 1.6 
+
+ 
+
+b=2.6 
+
+ 
+
+h = 0.1 
+
+ 
+
+x=np.arange(x0,b+h,h) 
+
+ 
+
+n=len(x)-1 
+
+ 
+
+y=np.empty(n+1) 
+
+ 
+
+y[0]= 4.6 
+
+ 
+
+for i in range(n): 
+
+ 
+
+    y[i+1]=y[i]+f(x[i],y[i])*h 
+
+ 
+
+    y_rounded=np.round (y,4) 
+
+ 
+
+print('x=',x, '\ny=',y_rounded) 
+
+ 
+
+plt.plot(x, y, 'o',x,y,'red') 
+
+ 
+
+plt.xlabel('x') 
+
+plt.ylabel('y') 
+
+ 
+
+plt.title('Mетод Eйлeра') 
+
+plt.legend([]) 
+
+plt.grid() 
+
+plt.show() 
