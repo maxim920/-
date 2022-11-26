b866b35aee00be6057f66ae93c8d7ccfcee8ed19
maxim920 <90177561+maxim920@users.noreply.github.com>
2022-11-26 23:55:03 +0200

    Update лб13_1.py

--- лб13_1.py
+++ лб13_1.py
@@ -32,6 +32,56 @@ h = 0.1
 
 x=np.arange(x0,b+h,h) 
 
+x=np.arange(x0,b+h,h) 
+
+ 
+
+n=len(x)-1 
+
+ 
+
+y=np.empty(n+1) 
+
+ 
+
+y[0]= 4.6 
+
+ 
+
+for i in range(n): 
+
+ 
+
+    y[i+1]=y[i]+(f(x[i],y[i])+f(x[i+1],y[i]+h*f(x[i],y[i])))*h/2 
+
+ 
+
+    y_rounded=np.round_(y,4) 
+
+ 
+
+print('x=',x, '\ny=',y_rounded) 
+
+ 
+
+plt.plot(x, y, 'o',x,y,'red') 
+
+ 
+
+plt.xlabel('x') 
+
+plt.ylabel('y') 
+
+ 
+
+plt.title('Mетод Eйлeра-Коші') 
+
+plt.legend([]) 
+
+plt.grid() 
+
+plt.show() 
+
  
 
 n=len(x)-1 
